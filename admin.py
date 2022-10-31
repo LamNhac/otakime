1411b43eb4308dee4bf99de125fc982e312f0fc8
LamNhac <ngodinhluan567@gmail.com>
2022-10-31 19:51:46 +0700

    add admin page and config url

--- admin.py
+++ admin.py
@@ -2,7 +2,39 @@ from flask import Flask,Blueprint,render_template
 
 admin = Blueprint('admin', __name__)
 
-@admin.route('/login')
+@admin.route('/admin')
 def login():
-    return 'Login'
+    title= "Otakime - Login"
+    return render_template('admin/login.html')
 
+@admin.route('/admin/create')
+def create():
+    title= "Otakime - Admin - Create"
+    return render_template(
+        'admin/create.html',
+       
+    )
+
+@admin.route('/admin/updatechapter')
+def updateChapter():
+    title= "Otakime - Admin - Update chapter"
+    return render_template(
+        'admin/updateChapter.html',
+       
+    )
+
+@admin.route('/admin/deletechapter')
+def deleteChapter():
+    title= "Otakime - Admin - Delete chapter"
+    return render_template(
+        'admin/deleteChapter.html',
+       
+    )
+
+@admin.route('/admin/deletemanga')
+def deleteManga():
+    title= "Otakime - Admin - Delete manga"
+    return render_template(
+        'admin/deleteManga.html',
+       
+    )
