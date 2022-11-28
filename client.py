f726a2214c70a2e7527c4d83bc1193f00834faac
ngodinhvu123 <ngodinhvuluan@gmail.com>
2022-11-28 12:53:11 +0700

    fix 404

--- client.py
+++ client.py
@@ -241,8 +241,5 @@ def movieScreen(urlMovie):
     else:
         return render_template('404.html')
 
-@client.errorhandler(404)
-def page_not_found(e):
-  
-    return render_template('404.html'), 404
+
 
