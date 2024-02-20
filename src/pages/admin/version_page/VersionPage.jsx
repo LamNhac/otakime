de3bf7817f96da635fa919c5571a82bb946c8e7d
LamNhac <ngodinhluan567@gmail.com>
2024-02-20 19:39:00 +0700

    add version page

--- src/pages/admin/version_page/VersionPage.jsx
+++ src/pages/admin/version_page/VersionPage.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
-
+import { Steps } from "antd";
+import React from "react";
+const description = "This is a description.";
 export default function VersionPage() {
   return (
-    <div>VersionPage</div>
-  )
+    <div>
+      <Steps
+        direction="vertical"
+        current={1}
+        items={[
+          {
+            title: "Version 1.0.0",
+            description: (
+              <div>
+                <p>Xây dựng giao diện client và admin</p>
+                <p>CRUD Manga và Movie</p>
+                <p>CRUD Category</p>
+                <p>Upload ảnh</p>
+              </div>
+            ),
+          },
+          {
+            title: "In Progress",
+            description,
+          },
+          {
+            title: "Waiting",
+            description,
+          },
+        ]}
+      />
+    </div>
+  );
 }
